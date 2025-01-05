52e8ff3d0e209e66bc8d047a007a3345340d4a50
nonepro <noneprobanru@gmail.com>
2025-01-05 11:39:46 +0800

    定义石头方块

--- mods/zsf_default/init.lua
+++ mods/zsf_default/init.lua
@@ -1,1 +1,9 @@
 core.log("action", "MOD: zsf_default")
+
+core.register_node("zsf_default:stone", {
+    description = "Stone",
+    tiles = {"default_stone.png"},
+    groups = {cracky=3},
+})
+
+core.register_alias("mapgen_stone", "zsf_default:stone")
